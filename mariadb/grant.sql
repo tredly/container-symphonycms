636b36aad23239f356de96a3027ca7dfe0eff685
Alistair Kearney <alistair@vuid.com>
2016-04-21 15:24:31 +1000

    Changed character set and collate properties of database created during installation

--- mariadb/grant.sql
+++ mariadb/grant.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS container_symphony2;
+CREATE DATABASE IF NOT EXISTS container_symphony2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 UPDATE mysql.user SET Password = PASSWORD('super_secret_password') WHERE User = 'root';
 grant all privileges on *.* to 'root'@'%' identified by 'super_secret_password' with grant option;
 FLUSH PRIVILEGES;
